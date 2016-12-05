b28216981e2a55559dbd9828149d010209281885
TheVTM <tallibba@gmail.com>
2016-12-05 19:47:17 -0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# CORS Proxy
+
+A simple [cors](https://www.html5rocks.com/en/tutorials/cors/) proxy I made using [koa](https://github.com/koajs/koa) for one of my projects.
+
+Feel free to use/fork it.
+
+It's very limited and only supports GET requests.
+
+## Usage
+
+### Server
+```
+git clone 
+npm install
+npm start
+```
+
+### Requests
+
+```
+curl http://localhost:3000/http://swapi.co/api/
+```
---
+++ server.js
@@ -0,0 +1,35 @@
+'use strict'
+
+/* LIBS */
+const koa = require('koa')
+const cors = require('kcors')
+const request = require('co-request')
+
+/* EXEC */
+const app = module.exports = koa()
+
+app.use(cors())
+
+app.use(function *(next) {
+  var start = new Date
+
+  yield next
+
+  var ms = new Date - start
+  this.set('X-Response-Time', ms + 'ms')
+})
+
+app.use(function *() {
+  const url = this.request.path.slice(1)
+  const response = yield request(url)
+
+  this.body = response.body
+  this.status = response.statusCode
+})
+
+if (!module.parent) {
+  const PORT = 3000
+
+  console.log(`Server is running in port: ${PORT}`)
+  app.listen(PORT)
+}
